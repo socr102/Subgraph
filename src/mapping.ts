5929e2ab8977e5e7d48616d8e5c6b4451a4a6407
socr102 <normanburtonfree@gmail.com>
2022-05-26 02:43:12 +0900

    initial subgraph

---
+++ src/mapping.ts
@@ -0,0 +1,18 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { Transfer } from "../generated/GraphToken/GraphToken"
+import { User } from "../generated/schema"
+
+export function handleTransfer(event: Transfer): void {
+  let user = User.load(event.params.from.toHexString());
+  if (user == null) {
+    user = new User(event.params.from.toHexString());
+    user.totalTransfers = BigInt.fromI32(0);
+    user.totalAmount = BigInt.fromI32(0);
+  }
+
+  user.totalTransfers = user.totalTransfers.plus(BigInt.fromI32(1));
+  user.totalAmount = user.totalAmount.plus(event.params.value);
+
+  user.save();
+
+}
